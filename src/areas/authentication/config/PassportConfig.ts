6cec873f9b7d42df6555e4bc5c474eb5c299e8cf
Tony Paik <tonypaik@Tonys-MacBook-Pro.local>
2023-03-04 11:05:29 -0800

    PassportConfig.ts

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,4 +3,26 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+import passport from "passport";
+import { Strategy } from "passport-local";
+
+export interface PassportStrategy {
+  name: string;
+  strategy: Strategy;
+}
+
+export default class PassportConfig {
+  constructor(strategies: PassportStrategy[] = []) {
+    this.addStrategies(strategies);
+  }
+
+  getaddStrategies(strategies: PassportStrategy[]) {
+    return this.addStrategies(strategies);
+  }
+
+  private addStrategies(strategies: PassportStrategy[]): void {
+    strategies.forEach((passportStrategy: PassportStrategy) => {
+      passport.use(passportStrategy.name, passportStrategy.strategy);
+    });
+  }
+}
