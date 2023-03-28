f4410f11d37984095b0d7840ce12e3a10c5508d4
Tony Paik <tonypaik@Tonys-MacBook-Pro.local>
2023-03-27 20:39:30 -0700

    delete work

--- src/areas/post/controllers/post.controller.ts
+++ src/areas/post/controllers/post.controller.ts
@@ -14,14 +14,13 @@ class PostController implements IController {
   private initializeRoutes() {
     this.router.get(this.path, this.getAllPosts);
     this.router.get(`${this.path}/:id`, this.getPostById);
-    this.router.get(`${this.path}/:id/delete`, this.deletePost);
+    this.router.post(`${this.path}/:id/delete`, this.deletePost);
     this.router.post(`${this.path}/:id/comment`, this.createComment);
     this.router.post(`${this.path}`, this.createPost);
   }
 
   // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary posts object
   private getAllPosts = (_: Request, res: Response) => {
-    
     res.render("post/views/posts", { posts: database.posts });
   };
 
@@ -34,7 +33,16 @@ class PostController implements IController {
   // 🚀 These post methods needs to be implemented by you
   private createComment = async (req: Request, res: Response, next: NextFunction) => {};
   private createPost = async (req: Request, res: Response, next: NextFunction) => {};
-  private deletePost = async (req: Request, res: Response, next: NextFunction) => {};
+  private deletePost = async (req: Request, res: Response, next: NextFunction) => {
+    const id = +req.params.id;
+    const post = database.posts[id - 1];
+    if (post) {
+      database.posts.splice(id - 1, 1);
+      res.redirect("/posts");
+    } else {
+      res.status(404).send("Post not found");
+    }
+  };
 }
 
 export default PostController;
